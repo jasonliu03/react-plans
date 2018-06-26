97bfc52c4c3d0f648696a591d720b91ea83a6685
guojin <guojing@zhiyuntcm.com>
2018-06-26 16:05:17 +0800

    create-react-app framework and Router

---
+++ src/components/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return(
+			<div>
+				<h2>首页</h2>
+				<p>REACT PLANS</p>
+				<br/>
+			</div>
+		)
+	}
+}
+
+export default Home
