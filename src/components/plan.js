662e94b81d9b869a6bd5ca7436bc5c38f3bd9034
guojin <guojing@zhiyuntcm.com>
2018-06-27 09:46:49 +0800

    add redux, change display:none to visibility:hidden

--- src/components/plan.js
+++ src/components/plan.js
@@ -1,33 +1,60 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import store from '../redux/store.js';
+import {show, deletePlan} from '../actions/plan.js';
+
 
 class Plan extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
+		console.log(props)
 	}
-	render(){
+  show () {
+    let b = this.props.planlist.show;
+    store.dispatch(show(!b));
+  }
+  delete (id) {
+  	store.dispatch(deletePlan(id));
+  }
+  detail (id) {
+  	this.props.history.push(`/detail/${id}`)
+  }
+	render () {
 		return (
-            <div>
-                <div className="plant">
-                    <h3>计划表</h3>
-                    <p>添加计划</p>
-                </div>
-                <table className="planlist">
-                    <thead>
-                        <tr>
-                            <th>标题</th>
-                            <th>操作</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td className="plan-title">计划1</td>
-                            <td className="plan-delect">删除</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        )
+			<div>
+				<div className="plant">
+					<h3>计划表</h3>
+					<p onClick={this.show.bind(this)}>添加计划</p>
+				</div>
+				<table className="planlist">
+					<thead>
+						<tr>
+							<th>标题</th>
+							<th>操作</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							this.props.planlist.planlist.map((item, index) => {
+								return (
+									<tr key={index}>
+										<td className="plan-title" onClick={this.detail.bind(this, item.id)}>{item.title}</td>
+										<td className="plan-delect" onClick={this.delete.bind(this, item.id)}>删除</td>
+									</tr>
+								)
+							})
+						}
+					</tbody>
+				</table>
+			</div>
+		)
 	}
 }
 
-export default Plan;
+const mapStateToProps = function(store) {
+  return {
+    planlist: store.planlist
+  };
+};
+
+export default connect(mapStateToProps)(Plan);
--- src/components/popup.js
+++ src/components/popup.js
@@ -1,35 +1,74 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import store from '../redux/store.js';
+import {show, addPlan} from '../actions/plan.js';
 
-class Pupop extends Component{
+class Popup extends Component{
   constructor (props) {
     super(props)
     this.state = {
       id: '',
-      title: '1',
-      content: '1'
+      title: '',
+      content: ''
     }
   }
+  // 取消按钮操作
+  close () {
+    console.log(this.props.planlist.show);
+    let b = this.props.planlist.show;
+    this.setState({
+      id: '',
+      title: '',
+      content: ''
+    })
+    store.dispatch(show(!b));
+  }
+  // 输入框事件
+  handleChage (str, e) {
+    this.setState({
+      id: Math.ceil(Math.random()*10000),
+      [str]: e.target.value
+    })
+  }
+  // 确认操作
+  conform () {
+    store.dispatch(addPlan(this.state));
+    this.setState({
+      id: '',
+      title: '',
+      content: ''
+    })
+    this.close();
+  }
+
   render() {
     let self = this;
     return (
-      <section className="popup" style={{display: 'block'}}>
+      <section className="popup" style={this.props.planlist.show ? {} : {visibility: 'hidden'}}>
         <div className="pbox">
-          <span className="close">X</span>
+          <span className="close" onClick={this.close.bind(this)}>X</span>
           <div>
             <h4>计划标题</h4>
-            <input value={this.state.title} placeholder="请输入计划标题"/>
+            <input onChange={this.handleChage.bind(this, 'title')} value={this.state.title} placeholder="请输入计划标题"/>
           </div>
           <div>
             <h4>计划内容</h4>
-            <textarea value={this.state.content} placeholder="请输入计划内容" rows="3"></textarea>
+            <textarea onChange={this.handleChage.bind(this, 'content')} value={this.state.content} placeholder="请输入计划内容" rows="3"></textarea>
           </div>
           <div className="pBtn">
-            <span>取消</span>
-            <span>确认</span>
+            <span onClick = {this.close.bind(this)}>取消</span>
+            <span onClick = {this.conform.bind(this)}>确认</span>
           </div>
         </div>
       </section>
     )
   }
 }
-export default Pupop
+
+const mapStateToProps = function(store) {
+  return {
+    planlist: store.planlist
+  };
+};
+// 连接 store和组件
+export default connect(mapStateToProps)(Popup);
